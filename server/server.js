9e9ecbca727625a438fcf8d83366377a11df5ff6
Shaik Mohammad anas <shaikanas2310@gmail.com>
2026-01-15 23:20:07 +0530

    Clean add client and server

--- server/server.js
+++ server/server.js
@@ -26,16 +26,16 @@ app.use("/api/dashboard", dashboardRoutes);
 
 // 🔹 Health Check
 app.get("/", (req, res) => {
-    res.send("Expense Tracker API is running 🚀");
+    res.send("Expense Tracker API is running");
 });
 
 // 🔹 MongoDB Connection
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("✅ MongoDB connected successfully");
+        console.log("MongoDB connected successfully");
     } catch (error) {
-        console.error("❌ MongoDB connection failed", error);
+        console.error(" MongoDB connection failed", error);
         process.exit(1);
     }
 };
@@ -45,6 +45,6 @@ const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`🚀 Server running on port ${PORT}`);
+        console.log(` Server running on port ${PORT}`);
     });
 });
